2ee209ad498f3b608389f142d6549297510e9c4a
sam wright <stwright9@gmail.com>
2019-11-18 16:04:49 -0500

    game with ui

---
+++ FallingBlocks/Assets/Scripts/FallingBlock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBlock : MonoBehaviour
+{
+    public Vector2 speedMinMax;
+    float speed;
+    float visibleHeightThershold;
+
+    private void Start()
+    {
+        speed = Mathf.Lerp(speedMinMax.x, speedMinMax.y, Difficulty.GetDifficultyPercent());
+
+        visibleHeightThershold = -Camera.main.orthographicSize - transform.localScale.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime, Space.Self);
+
+        if(transform.position.y < visibleHeightThershold)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
